bde5c8936b976180bf20faa923de2c7477697f74
Chourouk Hlaoui <61617827+chouhlaoui@users.noreply.github.com>
2024-05-04 23:23:12 +0100

    Final

--- app/src/main/java/com/example/tv_app/MainActivity.kt
+++ app/src/main/java/com/example/tv_app/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.tv_app
 
 import android.content.ContentValues.TAG
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
